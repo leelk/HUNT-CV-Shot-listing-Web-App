1e9dc75d01024b9d565d98ab64eb970f63e859a7
leelk <karunarathne1995@gmail.com>
2019-03-04 10:22:54 +0530

    Initiate

---
+++ resources/views/components/who.blade.php
@@ -0,0 +1,26 @@
+    @if(Auth::guard('web')->check())
+
+    <p class="text-success">
+        You are Logged in as <strong>USER</strong>
+    </p>
+
+    @else
+
+    <p class="text-danger">
+        You are Logged out as a <strong>USER</strong>
+    </p>
+    @endif
+
+
+    @if(Auth::guard('admin')->check())
+
+        <p class="text-success">
+            You are Logged in as <strong>Admin</strong>
+        </p>
+
+    @else
+
+        <p class="text-danger">
+            You are Logged out as a <strong>ADMIN</strong>
+        </p>
+    @endif
