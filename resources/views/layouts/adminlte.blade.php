aee800b0b270b82969022acfbf44ca2ce9483d36
leelk <karunarathne1995@gmail.com>
2019-03-19 01:31:27 +0530

    Admin Dashboard icon Added

--- resources/views/layouts/adminlte.blade.php
+++ resources/views/layouts/adminlte.blade.php
@@ -89,6 +89,78 @@
         <!-- Main content -->
         <section class="content container-fluid">
 
+            <section class="content">
+                <!-- Info boxes -->
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-xs-12" >
+                        <div class="info-box">
+                            <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline">
+                                    <img src="https://bit.ly/2TNXIIr" width="65px" height="65px" alt="">
+                                </i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Create New <br> <STRONG>Vacancy</STRONG></span>
+                                <a href="#" class="small-box-footer">Click Here<i class="fa fa-arrow-circle-right"></i></a>
+                                {{--<span class="info-box-number">90<small>%</small></span>--}}
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-red"><i class="fa fa-google-plus">
+                                    <img src="https://bit.ly/2Tfy0YR" height="65px" width="65px" alt="">
+                                </i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">All Vacancies</span>
+                                {{--<span class="info-box-number">41,410</span>--}}
+                                <a href="#" class="small-box-footer">Click Here <i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+
+                    <!-- fix for small devices only -->
+                    <div class="clearfix visible-sm-block"></div>
+
+                    <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Sales</span>
+                                <span class="info-box-number">760</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">New Members</span>
+                                <span class="info-box-number">2,000</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+
+
+
+            </section>
+
             <!--------------------------
               | Your Page Content Here |
               -------------------------->
